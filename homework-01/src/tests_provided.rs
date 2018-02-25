21cef9ae3e108528ff1137bcb21f9816968a76d0
Dmitry Shur <dimashur@gmail.com>
2018-02-24 19:56:55 -0500

    finished the whole first problem

--- homework-01/src/tests_provided.rs
+++ homework-01/src/tests_provided.rs
@@ -25,17 +25,17 @@ fn test_dedup_small() {
     assert_eq!(dedup(&vs), vec![1,2,3,4]);
 }
 
-//// Part 3
+// Part 3
 
-//fn even_predicate(x: i32) -> bool {
-    //(x % 2) == 0
-//}
+fn even_predicate(x: i32) -> bool {
+    (x % 2) == 0
+}
 
-//#[test]
-//fn test_filter_small() {
-    //let vs = vec![1,2,3,4,5];
-    //assert_eq!(filter(&vs, &even_predicate), vec![2,4]);
-//}
+#[test]
+fn test_filter_small() {
+    let vs = vec![1,2,3,4,5];
+    assert_eq!(filter(&vs, &even_predicate), vec![2,4]);
+}
 
 ////
 //// Problem 2
