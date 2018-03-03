c965de3aae26259cda1dd579151e8beb4fb06bde
Dmitry Shur <dimashur@gmail.com>
2018-03-03 15:08:11 -0500

    solved the 3rd problem

--- homework-01/target/rls/debug/deps/homework_01-43cda7e249a65eff.d
+++ homework-01/target/rls/debug/deps/homework_01-43cda7e249a65eff.d
@@ -1,8 +1,9 @@
-/home/shurikdima/programming/rust/rust-course/homework-01/target/rls/debug/deps/homework_01-43cda7e249a65eff.rmeta: src/lib.rs src/problem1.rs src/problem2.rs src/tests_provided.rs
+/home/shurikdima/programming/rust/rust-course/homework-01/target/rls/debug/deps/homework_01-43cda7e249a65eff.rmeta: src/lib.rs src/problem1.rs src/problem2.rs src/problem3.rs src/tests_provided.rs
 
-/home/shurikdima/programming/rust/rust-course/homework-01/target/rls/debug/deps/homework_01-43cda7e249a65eff.d: src/lib.rs src/problem1.rs src/problem2.rs src/tests_provided.rs
+/home/shurikdima/programming/rust/rust-course/homework-01/target/rls/debug/deps/homework_01-43cda7e249a65eff.d: src/lib.rs src/problem1.rs src/problem2.rs src/problem3.rs src/tests_provided.rs
 
 src/lib.rs:
 src/problem1.rs:
 src/problem2.rs:
+src/problem3.rs:
 src/tests_provided.rs:
