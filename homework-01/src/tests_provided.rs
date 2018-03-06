b95c2e6acdfa87554e70c5fac43ddd60da6c384c
Dmitry Shur <dimashur@gmail.com>
2018-03-06 16:10:52 -0500

    finished 1st homework

--- homework-01/src/tests_provided.rs
+++ homework-01/src/tests_provided.rs
@@ -92,13 +92,13 @@ fn test_sieve_basic() {
     assert_eq!(vec![2, 3, 5, 7, 11], sieve(12));
 }
 
+
+// Problem 4
 //
-//// Problem 4
-////
 
 //#[test]
 //fn test_hanoi_1_disks() {
-//let result = hanoi(1, Peg::A, Peg::B, Peg::C);
-//assert_eq!(vec![(Peg::A, Peg::C)], result);
-//assert_eq!(1, result.len());
+    //let result = hanoi(1, Peg::A, Peg::B, Peg::C);
+    //assert_eq!(vec![(Peg::A, Peg::C)], result);
+    //assert_eq!(1, result.len());
 //}
